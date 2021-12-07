1fe6064ff920c3ce593f8da1d7dd8f10a14a778f
PurpleYellow36 <sawadanaokipg@gmail.com>
2021-12-08 02:20:26 +0900

    check error commit

--- pcbuild_calc/settings.py
+++ pcbuild_calc/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+wor8v)f(s*%lcwnj84e_o8qm)sa_5gg0khg5(8--6bhci7xwn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pcbuildcalc.herokuapp.com']
 
