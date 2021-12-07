6d5193ab359eb451ea19a35380aba0f325e417e2
PurpleYellow36 <sawadanaokipg@gmail.com>
2021-12-08 02:17:01 +0900

    try resolve css problem commit

--- pcbuild_calc/settings.py
+++ pcbuild_calc/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pcbuild_calc.urls'
@@ -130,7 +131,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
- os.path.join(BASE_DIR,'staticfiles'),
+ os.path.join(BASE_DIR,'static'),
 )
 
 # Default primary key field type
