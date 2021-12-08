7e748a38f49486a631667d2bcea8e3960b9a192a
PurpleYellow36 <sawadanaokipg@gmail.com>
2021-12-08 18:09:10 +0900

    heroku deploy commit

--- pcbuild_calc/settings.py
+++ pcbuild_calc/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-+wor8v)f(s*%lcwnj84e_o8qm)sa_5gg0khg5(8--6bhci7xwn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://pcbuildcalc.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['pcbuildcalc.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
